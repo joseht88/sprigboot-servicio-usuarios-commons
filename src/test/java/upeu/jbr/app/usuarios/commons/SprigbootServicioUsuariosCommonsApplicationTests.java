ba01dc54d2241b2feb59a02746f49edd4471039d
joseht8 <joseht8@gmail.com>
2022-07-11 21:11:50 -0500

    first commit

---
+++ src/test/java/upeu/jbr/app/usuarios/commons/SprigbootServicioUsuariosCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.jbr.app.usuarios.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigbootServicioUsuariosCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
